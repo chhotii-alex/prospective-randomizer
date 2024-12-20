cef1eb98725049aed39eb6b2d98e7c8c97d7c3b0
Alex Morgan <lexyym@gmail.com>
2024-12-20 07:13:40 -0500

    template Spring Boot application from Initializr

---
+++ pros_rand/src/main/java/org/sleepandcognition/pros_rand/ProsRandApplication.java
@@ -0,0 +1,13 @@
+package org.sleepandcognition.pros_rand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsRandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProsRandApplication.class, args);
+	}
+
+}
---
+++ pros_rand/src/test/java/org/sleepandcognition/pros_rand/ProsRandApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sleepandcognition.pros_rand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsRandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
