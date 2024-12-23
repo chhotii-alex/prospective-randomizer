b60f5775b9b99efca9832da2daf2500a14ea5384
Alex Morgan <lexyym@gmail.com>
2024-12-23 11:38:20 -0500

    dev note

--- pros-rand-boot/src/main/java/org/sleepandcognition/prosrandboot/ProsRandApplication.java
+++ pros-rand-boot/src/main/java/org/sleepandcognition/prosrandboot/ProsRandApplication.java
@@ -5,6 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProsRandApplication {
+    /*
+     * Some development notes:
+     * Maintain consistent source code formatting using the maven "spotless" plug-in. 
+     * This is configured for each of the modules (but not for the aggregated project,
+     * as that has no code). To invoke, cd into the directory of whatever module has
+     * been edited, then invoke mvn, i.e.:
+     * cd pros-rand-boot
+     * mvn spotless:apply
+     */
 
     public static void main(String[] args) {
         SpringApplication.run(ProsRandApplication.class, args);
