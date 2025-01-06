414cee4d98d3623a344a09d554ec0a981f4d4cab
Alex Morgan <lexyym@gmail.com>
2025-01-06 16:54:06 -0500

    working on simulation

--- simulation.py
+++ simulation.py
@@ -9,11 +9,21 @@ random.seed(42)
 
 protocols = {
     "simpletest" : {
-        'groupNames': ['C', 'D'],
+        'groupNames': ['A', 'B'],
         'variableSpec': {'score': None},
-        'allowRevision': True,
-        'algorithm': 'Balanced',
-    }
+        'allowRevision': False,
+    },
+    "multivar": {
+        'groupNames': ['A', 'B'],
+        'variableSpec': {'iq': None,
+                         'shoesize': None},
+        'allowRevision': False,
+    },
+    "moregroups": {
+        'groupNames': ['A', 'B', 'C', 'D'],
+        'variableSpec': {'score': None},
+        'allowRevision': False,
+    },
 }
 
 def setup():
@@ -46,15 +56,19 @@ def start_protocol(protocol_name, pid):
     assert r.status_code == 200
 
 # This submits feature values but doesn't demand immediate group assignment
-def put_random_subject(subjectID, protocol_name, pid):
+def put_random_subject(subjectID, protocol_name, pid, features=None):
+    if features is None:
+        features = make_features_for(protocol_name)
     r = requests.post(make_url(protocol_name, pid, ['subject', subjectID]),
-                      json=make_features_for(protocol_name))
+                      json=features)
     assert r.status_code == 200
 
 # This submits feature values and demands immediate group assignment
-def place_random_subject(subjectID, protocol_name, pid):
+def place_random_subject(subjectID, protocol_name, pid, features=None):
+    if features is None:
+        features = make_features_for(protocol_name)
     r = requests.post(make_url(protocol_name, pid, ['subject', subjectID, 'group']),
-                      json=make_features_for(protocol_name))
+                      json=features)
     assert r.status_code == 200
 
 # Trigger assignment of this one subject specifically (although might trigger
@@ -87,30 +101,37 @@ setup();
 for pid in pid_gen():
     for protocol_name in protocols.keys():
         for placing in [True, False]:
-            for algorithm in ['Alternating', 'Balanced']:
-                protocols[protocol_name]['algorithm'] = algorithm
-                prot_suff = pid + algorithm + str(placing)
-                start_protocol(protocol_name, prot_suff)
+            for max_subject_count in [12, 24, 36]:
+                competitors = {}
+                for algorithm in ['Alternating', 'Balanced']:
+                    protocols[protocol_name]['algorithm'] = algorithm
+                    prot_suff = pid + algorithm + str(placing) + str(max_subject_count)
+                    start_protocol(protocol_name, prot_suff)
+                    competitors[algorithm] = prot_suff
                 put_subjects = []
                 for s_id in id_gen():
-                    if placing:
-                        place_random_subject(s_id, protocol_name, prot_suff)
-                    else:
-                        put_random_subject(s_id, protocol_name, prot_suff)
+                    features = make_features_for(protocol_name)
+                    for prot_suff in competitors.values():
+                        if placing:
+                            place_random_subject(s_id, protocol_name, prot_suff, features)
+                        else:
+                            put_random_subject(s_id, protocol_name, prot_suff, features)
                     put_subjects.append(s_id)
-                    if (len(put_subjects) >= 12):
+                    if (len(put_subjects) >= max_subject_count):
                         break
-                assign_all(protocol_name, prot_suff)
-                groups = get_groups(protocol_name, prot_suff)
-                (min_group_size, max_group_size) = (999999, 0)
-                for group in groups:
-                    group_size = len(group['subjects'])
-                    if group_size < min_group_size:
-                        min_group_size = group_size
-                    if group_size > max_group_size:
-                        max_group_size = group_size
-                pp(groups)
-                assert (max_group_size - min_group_size) <= 1
+                    
+                for prot_suff in competitors.values():
+                    assign_all(protocol_name, prot_suff)
+                    groups = get_groups(protocol_name, prot_suff)
+                    (min_group_size, max_group_size) = (999999, 0)
+                    for group in groups:
+                        group_size = len(group['subjects'])
+                        if group_size < min_group_size:
+                            min_group_size = group_size
+                        if group_size > max_group_size:
+                            max_group_size = group_size
+                    pp(groups)
+                    assert (max_group_size - min_group_size) <= 1
 
                 
         
