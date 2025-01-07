8f622a64d5f7e0a83ce54a5c5b92642ee98db8a1
Alex Morgan <lexyym@gmail.com>
2025-01-07 18:17:35 -0500

    detail

--- analyze.py
+++ analyze.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 df = pd.read_excel('results.xlsx')
-summ = df.groupby(['algorithm', 'place_interval'])['pvalue'].mean()
+summ = df.groupby(['algorithm', 'n_vars', 'place_interval'])['pvalue'].mean()
 
 print(summ)
